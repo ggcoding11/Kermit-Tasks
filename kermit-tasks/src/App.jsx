53b8f43385ff2a043290ea411efc7038155f08b5
Gustavo <gustavogomes1108@gmail.com>
2025-12-31 09:51:16 -0300

    .

--- kermit-tasks/src/App.jsx
+++ kermit-tasks/src/App.jsx
@@ -9,36 +9,33 @@ const App = () => {
     >
       <div className="card card-principal">
         <div className="container">
-          <header className="d-flex justify-content-center gap-2">
+          <header className="d-flex gap-2">
             <img src="../photos/logo.png" alt="logo" />
             <h1 className="title">Kermit Tasks</h1>
           </header>
 
-          <section>
-            <div className="tasks-progress row">
-              <div className="col-6">
-                <p>Tasks progreess</p>
-
-                <div
-                  class="progress"
-                  role="progressbar"
-                  aria-label="Basic example"
-                  aria-valuenow="25"
-                  aria-valuemin="0"
-                  aria-valuemax="100"
-                >
-                  <div class="progress-bar w-25"></div>
+          <section className="d-flex justify-content-center mt-4">
+            <div className="task-progress bg-secondary p-2">
+              <div className="row">
+                <div className="col-9 d-flex justify-content-center align-items-center">
+                  <div
+                    className="progress"
+                    role="progressbar"
+                    aria-label="Basic example"
+                    aria-valuenow="50"
+                    aria-valuemin="0"
+                    aria-valuemax="100"
+                  >
+                    <div className="progress-bar w-50"></div>
+                  </div>
                 </div>
-              </div>
-
-              <div className="col-6">
-                <div className="tasks-completed d-flex justify-content-center align-items-center">
-                  <span className="fw-semibold">1/3</span>
+                <div className="col-3">
+                  <div className="tasks-completed d-flex justify-content-center align-items-center">
+                    <span className="fw-bold">1/3</span>
+                  </div>
                 </div>
               </div>
             </div>
-
-            <div className="tasks-list"></div>
           </section>
 
           <footer></footer>
