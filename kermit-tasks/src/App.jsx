af4ac9c84ac254742fbdfb7068a1d152f59d3881
Gustavo <gustavogomes1108@gmail.com>
2026-01-06 12:50:40 -0300

    .

--- kermit-tasks/src/App.jsx
+++ kermit-tasks/src/App.jsx
@@ -44,98 +44,104 @@ const App = () => {
       className="container-fluid min-vh-100 d-flex justify-content-center align-items-center"
       id="main"
     >
-      <div className="card card-principal p-3">
-        <div className="container">
-          <header className="d-flex justify-content-center align-items-center gap-2 mt-2">
-            <img src="../photos/logo.png" alt="logo" />
-            <h1 className="title">Kermit Tasks</h1>
-          </header>
+      <div className="row d-flex justify-content-center w-100">
+        <div className="col-12 col-sm-9 col-md-6 col-lg-4">
+          <div className="card card-principal p-3">
+            <div className="container">
+              <header className="d-flex justify-content-center align-items-center gap-2 mt-2">
+                <img src="../photos/logo.png" alt="logo" />
+                <h1 className="title">Kermit Tasks</h1>
+              </header>
 
-          <section className="d-flex justify-content-center flex-column align-items-center mt-2">
-            <div className="form-group w-100">
-              <form onSubmit={addTask}>
-                <div className="input-group mb-3">
-                  <span className="input-group-text border border-secondary">
-                    📝
-                  </span>
-                  <input
-                    type="text"
-                    name=""
-                    id="task-name"
-                    className="form-control border border-secondary"
-                    onChange={(e) => {
-                      setTaskName(e.target.value);
-                    }}
-                    value={taskName}
-                    placeholder="Write the task name..."
-                    required
-                  />
-                </div>
-
-                <div className="input-group mb-3">
-                  <span className="input-group-text border border-secondary">
-                    ⏰
-                  </span>
-                  <input
-                    type="number"
-                    name=""
-                    id="pomodoro-number"
-                    min={1}
-                    className="form-control border border-secondary"
-                    onChange={(e) => {
-                      setPomodoro(e.target.value);
-                    }}
-                    value={pomodoro}
-                    placeholder="How many pomodoros?"
-                    required
-                  />
-                </div>
-
-                <div className="d-flex justify-content-center">
-                  <button type="submit" className="btn btn-primary w-100">
-                    Add task
-                  </button>
-                </div>
-              </form>
-            </div>
-
-            <div className="tasks-list w-100">
-              <div className="d-flex justify-content-center align-items-center flex-column">
-                {taskList.map((item) => (
-                  <div
-                    key={item.id}
-                    className="task d-flex justify-content-between align-items-center border border-secondary rounded-4 w-100 gap-4 p-2 mt-3"
-                    onClick={() => checkTask(item.id)}
-                  >
-                    <div className="d-flex gap-2">
-                      {item.completed === false ? (
-                        <i className="bi bi-circle"></i>
-                      ) : (
-                        <i className="bi bi-check-circle-fill"></i>
-                      )}
-                      <span className="fw-bold">{item.name}</span>
+              <section className="d-flex justify-content-center flex-column align-items-center mt-2">
+                <div className="form-group w-100">
+                  <form onSubmit={addTask}>
+                    <div className="input-group mb-3">
+                      <span className="input-group-text border border-secondary">
+                        📝
+                      </span>
+                      <input
+                        type="text"
+                        name=""
+                        id="task-name"
+                        className="form-control border border-secondary"
+                        onChange={(e) => {
+                          setTaskName(e.target.value);
+                        }}
+                        value={taskName}
+                        placeholder="Write the task name..."
+                        required
+                      />
                     </div>
 
-                    <div className="d-flex align-items-center gap-2">
-                      <button className="btn btn-primary btn-sm">
-                        <i className="bi bi-pencil-square"></i>
-                      </button>
+                    <div className="input-group mb-3">
+                      <span className="input-group-text border border-secondary">
+                        ⏰
+                      </span>
+                      <input
+                        type="number"
+                        name=""
+                        id="pomodoro-number"
+                        min={1}
+                        className="form-control border border-secondary"
+                        onChange={(e) => {
+                          setPomodoro(e.target.value);
+                        }}
+                        value={pomodoro}
+                        placeholder="How many pomodoros?"
+                        required
+                      />
+                    </div>
 
-                      <button className="btn btn-danger btn-sm">
-                        <i className="bi bi-x"></i>
+                    <div className="d-flex justify-content-center">
+                      <button type="submit" className="btn btn-primary w-100">
+                        Add task
                       </button>
+                    </div>
+                  </form>
+                </div>
 
-                      <div className="bg-secondary p-1 rounded-circle">
-                        <span className="text-white fw-bold">0/{item.pomodoros}</span>
+                <div className="tasks-list w-100">
+                  <div className="d-flex justify-content-center align-items-center flex-column">
+                    {taskList.map((item) => (
+                      <div
+                        key={item.id}
+                        className="task d-flex justify-content-between align-items-center border border-secondary rounded-4 w-100 gap-4 p-2 mt-3"
+                        onClick={() => checkTask(item.id)}
+                      >
+                        <div className="d-flex gap-2">
+                          {item.completed === false ? (
+                            <i className="bi bi-circle"></i>
+                          ) : (
+                            <i className="bi bi-check-circle-fill"></i>
+                          )}
+                          <span className="fw-bold">{item.name}</span>
+                        </div>
+
+                        <div className="d-flex align-items-center gap-2">
+                          <button className="btn btn-primary btn-sm">
+                            <i className="bi bi-pencil-square"></i>
+                          </button>
+
+                          <button className="btn btn-danger btn-sm">
+                            <i className="bi bi-x"></i>
+                          </button>
+
+                          <div className="bg-secondary p-1 rounded-circle">
+                            <span className="text-white fw-bold">
+                              0/{item.pomodoros}
+                            </span>
+                          </div>
+                        </div>
                       </div>
-                    </div>
+                    ))}
                   </div>
-                ))}
-              </div>
-            </div>
-          </section>
+                </div>
+              </section>
 
-          <footer className="d-flex justify-content-center"></footer>
+              <footer className="d-flex justify-content-center"></footer>
+            </div>
+          </div>
         </div>
       </div>
     </div>
